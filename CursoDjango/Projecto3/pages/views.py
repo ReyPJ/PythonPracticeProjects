91852ca12094d3a2264e2b29c7f8af22a480ec18
Reyner <reyenr1012002@gmail.com>
2024-03-10 02:28:51 -0600

    CRUD Finish

--- CursoDjango/Projecto3/pages/views.py
+++ CursoDjango/Projecto3/pages/views.py
@@ -1,8 +1,9 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .forms import PageForm
 
 
 # Create your views here.
@@ -16,5 +17,19 @@ class PageDetailView(DetailView):
 
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm
+    success_url = reverse_lazy('pages:pages')
+
+
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class PageDeleteView(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')
