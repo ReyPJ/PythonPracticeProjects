381ba6d8309977076433dc59fbf7ba31323d8890
Reyner <reyenr1012002@gmail.com>
2024-03-27 18:05:21 -0600

    adding some stuff

--- CursoDjango/Projecto3/pages/views.py
+++ CursoDjango/Projecto3/pages/views.py
@@ -1,9 +1,22 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .forms import PageForm
+
+
+class StaffRequiredMixin(object):
+    """
+    Custom mixin
+    """
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
 # Create your views here.
@@ -15,12 +28,14 @@ class PageDetailView(DetailView):
     model = Page
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
@@ -30,6 +45,7 @@ class PageUpdateView(UpdateView):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDeleteView(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
